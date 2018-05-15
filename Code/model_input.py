2eb009713bd49ec0cb981bf9dd51de22d80216a8
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2018-05-15 11:06:28 +0200

    Added GPU support and modified net

--- Code/model_input.py
+++ Code/model_input.py
@@ -6,35 +6,100 @@ import TFRecord_dataset as exp_TFR
 from extract_features import extract_features
 
 
+class InputInitHook(tf.train.SessionRunHook):
+    pass
+    # def begin(self):
+    #     self.tables_initializer = tf.tables_initializer()
+    #
+    # def after_create_session(self, session, coord):
+    #     session.run(self.tables_initializer)
+
+
+def create_fully_connected_layer(net, units, activation, name, is_training):
+    net = tf.layers.dense(net,  # input
+                          units=units,  # number of neurons
+                          use_bias=False,
+                          # activation=tf.nn.sigmoid, # activation function
+                          activation=None,
+                          name=name)
+
+    if activation is not None:
+        net = tf.layers.batch_normalization(net, axis=-1, training=is_training, fused=True)
+        net = activation(net)
+
+    return net
+
+
 def create_input_fn(filename):
+    # init_hook = InputInitHook()
+
     def input_fn():
         # Load and parse dataset
         dataset = tf.data.TFRecordDataset(filename, compression_type='GZIP')
-        corpus = dataset.map(exp_TFR.decode)
+        corpus = dataset.map(exp_TFR.decode, num_parallel_calls=8)
+
+        corpus = corpus.shuffle(5000000)
+
+        # Build the dictionary
+        # Extract the top 60000 most common words to include in our embedding vector
+        vocab_file_path = "Dataset/Vocabulary/vocabulary.txt"
+        vocab_size = 60000
+
+        # Gather together all the unique words and index them with a unique integer value
+        # Loop through every word in the dataset and assign it to the unique integer word identified.
+        # Any words not within the top 60000 most common words will be marked with "-1" and replace with "UNK" token
+
+        # Load the dictionary populated by keys corresponding to each unique word
+        table = tf.contrib.lookup.index_table_from_file(vocabulary_file=vocab_file_path,
+                                                        vocab_size=vocab_size,
+                                                        key_column_index=1,
+                                                        delimiter=' ')
+
+        # Create a reverse_table that allows us to look up a word based on its unique integer identifier,
+        # rather than looking up the identifier based on the word.
+        # reverse_table = tf.contrib.lookup.index_to_string_table_from_file(vocabulary_file=vocab_file_path,
+        #                                                                   vocab_size=vocab_size,
+        #                                                                   value_column_index=1,
+        #                                                                   delimiter=' ')
+
+        # Load ocean dictionary
+        ocean_dict_file_path = "Dataset/Vocabulary/ocean_dict_filtered.txt"
+        ocean_dict_size = 634  # 636 before (deleted 2 adjective)
+
+        # Ocean lookup-table
+        ocean_table = tf.contrib.lookup.index_table_from_file(vocabulary_file=ocean_dict_file_path,
+                                                              vocab_size=ocean_dict_size,
+                                                              key_column_index=0,
+                                                              delimiter='\t')
 
         # Extract labels and features and generate dataset
-        dataset = corpus.map(extract_features)
+        dataset = corpus.map(lambda ids, text: extract_features(ids, text, table, ocean_table), num_parallel_calls=8)
+
+        # Delete all the sentences without adjective
+        dataset = dataset.filter(lambda features, ocean_vector: tf.reduce_all(tf.is_finite(ocean_vector)))
+
+        dataset = dataset.batch(batch_size=500)
 
         return dataset
-    
-    return input_fn
+
+    return input_fn  # , init_hook
 
 
 def model_fn(features, labels, mode, params):
+    is_training = (mode == tf.estimator.ModeKeys.TRAIN)
+
     # FIXME: mancano i params
 
     # Neural Network
     net = features['bow_vector']
 
-    net = tf.layers.dense(net,                       # input
-                          units=20,                  # number of neurons
-                          activation=tf.nn.sigmoid,  # activation function
-                          name='layer1')
+    net = create_fully_connected_layer(net, 100, tf.nn.relu, 'layer1', is_training)
 
-    net = tf.layers.dense(net,              # input
-                          units=5,          # number of neurons
-                          activation=None,  # activation function (linear output)
-                          name='layer2')
+    net = create_fully_connected_layer(net, 50, tf.nn.relu, 'layer2', is_training)
+
+    net = create_fully_connected_layer(net, 20, tf.nn.relu, 'layer3', is_training)
+
+    net = create_fully_connected_layer(net, 5, None, 'layer4', is_training)
 
     # Predictions
     if mode == tf.estimator.ModeKeys.PREDICT:
@@ -46,18 +111,65 @@ def model_fn(features, labels, mode, params):
     loss = tf.losses.mean_squared_error(labels=labels,
                                         predictions=net)
 
-    metric_ops = {'rmse': tf.sqrt(loss)}
+    # loss = tf.reduce_mean(
+    #     tf.nn.nce_loss(weights=nce_weights,
+    #                    biases=nce_biases,
+    #                    labels=labels,
+    #                    inputs=embed,
+    #                    num_sampled=num_sampled,
+    #                    num_classes=vocabulary_size))
 
-    tf.summary.scalar('rmse', tf.sqrt(loss))     # Tensorboard
+    # loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=labels, predictions=net)
+
+    rmse = tf.metrics.root_mean_squared_error(labels,
+                                              net)
+
+    rmse_o = tf.metrics.root_mean_squared_error(labels[:, 0],
+                                                net[:, 0])
+
+    rmse_c = tf.metrics.root_mean_squared_error(labels[:, 1],
+                                                net[:, 1])
+
+    rmse_e = tf.metrics.root_mean_squared_error(labels[:, 2],
+                                                net[:, 2])
+
+    rmse_a = tf.metrics.root_mean_squared_error(labels[:, 3],
+                                                net[:, 3])
+
+    rmse_n = tf.metrics.root_mean_squared_error(labels[:, 4],
+                                                net[:, 4])
+
+    metric_ops = {'rmse': rmse, 'rmse_openness': rmse_o, 'rmse_conscientiousness': rmse_c,
+                  'rmse_extraversion': rmse_e, 'rmse_agreeableness': rmse_a, 'rmse_neuroticism': rmse_n}
+
+    tf.summary.scalar('rmse', rmse[1])  # Tensorboard
+
+    tf.summary.scalar('rmse_openness', rmse_o[1])  # Tensorboard
+    tf.summary.scalar('rmse_conscientiousness', rmse_c[1])  # Tensorboard
+    tf.summary.scalar('rmse_extraversion', rmse_e[1])  # Tensorboard
+    tf.summary.scalar('rmse_agreeableness', rmse_a[1])  # Tensorboard
+    tf.summary.scalar('rmse_neuroticism', rmse_n[1])  # Tensorboard
 
     if mode == tf.estimator.ModeKeys.EVAL:
-        return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=metric_ops)
+        return tf.estimator.EstimatorSpec(mode, loss=loss,
+                                          eval_metric_ops=metric_ops)
 
     # Training
     assert mode == tf.estimator.ModeKeys.TRAIN
 
-    optimizer = tf.train.AdagradDAOptimizer(learning_rate=0.9)
+    # compute mean cross entropy (softmax is applied internally)
+    # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=py_x, labels=Y))
 
+    # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=predictions, labels=labels))
+    # predictions = multilayer_perceptron(x, weights, biases, keep_prob)
+
+    # train_op = tf.train.GradientDescentOptimizer(0.05).minimize(cost)  # construct optimizer
+    # predict_op = tf.argmax(py_x, 1)  # at predict time, evaluate the argmax of the logistic regression
+
+    # check_op = tf.add_check_numerics_ops()
+
+    # with tf.control_dependencies([check_op]):
+    optimizer = tf.train.AdagradOptimizer(learning_rate=0.0001)
     train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
 
     return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
--- Code/training.py
+++ Code/training.py
@@ -2,29 +2,79 @@
 # -*- coding: utf-8 -*-
 
 import tensorflow as tf
-from model_input import create_input_fn
 from model_input import model_fn
+from model_input import create_input_fn
 
 
 def main(argv):
+
+    train_data = "Dataset/TFRecords/train.tfrecords.gz"
+    test_data = "Dataset/TFRecords/test.tfrecords.gz"
+
     session_config = tf.ConfigProto()
 
-    config = tf.estimator.RunConfig(model_dir='Model/',
+    config = tf.estimator.RunConfig(model_dir='Model/003/',
                                     save_summary_steps=50,
-                                    session_config = session_config)
+                                    session_config=session_config,
+                                    # log_step_count_steps=1
+                                    )
 
     estimator = tf.estimator.Estimator(model_fn=model_fn,
                                        params={},
                                        config=config)
 
-    hooks = [tf.train.ProfilerHook(save_steps=200, output_dir='Timeline/')]
+    input_fn = create_input_fn(train_data)
 
-    estimator.train(input_fn=create_input_fn("Dataset/TFRecords/train.tfrecords.gz"),
+    hooks = [tf.train.ProfilerHook(save_steps=200, output_dir='Timeline/')]  # init_hook]
+
+    idx = 0
+
+    for idx in range(0, 10):
+        estimator.train(input_fn=input_fn,
                     hooks=hooks)
 
-    eval_result = estimator.evaluate(input_fn=create_input_fn("Dataset/TFRecords/test.tfrecords.gz"))
+        eval_result = estimator.evaluate(input_fn=create_input_fn("Dataset/TFRecords/test.tfrecords.gz"))
+        print('\nRoot Mean Square Error: {rmse:0.3f}\n'.format(**eval_result))
 
-    print('\nRoot Mean Square Error: {rmse:0.3f}\n'.format(**eval_result))
+        idx += 1
+
+    predictions = estimator.predict(input_fn=create_input_fn(test_data))
+
+    input = create_input_fn(test_data)
+    dataset = input()
+    iterator = dataset.make_initializable_iterator()
+    feature, label = iterator.get_next()
+
+    file = open("Model_Dataset/prediction_model_003.txt", "w")
+    ids = 0
+
+    with tf.Session() as sess:
+        tf.tables_initializer().run()
+        sess.run(iterator.initializer)
+
+        while True:
+            try:
+                label_ = sess.run(label)
+
+                for l in label_:
+                    predicted = predictions.__next__()
+                    file.write('id:')
+                    file.write(str(ids))
+                    file.write('\t')
+                    file.write('real: ')
+                    file.write(str(l))
+                    file.write('\t')
+                    file.write('predicted: ')
+                    file.write(str(predicted['ocean']))
+                    file.write('\n')
+
+                    ids += 1
+
+                if ids % 500 == 0:
+                    print(ids)
+            except tf.errors.OutOfRangeError:
+                break
+    file.close()
 
 
 if __name__ == '__main__':
